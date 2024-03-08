e36cd6a4f4b66603f608417c157257a922b6494f
NischayHR-11 <nischayhr11@gmail.com>
2024-03-09 00:05:39 +0530

    DELETION FUNCTION ADDED IN TRIE DATA STRUCTURE

--- Tree/TrieTree/trie.java
+++ Tree/TrieTree/trie.java
@@ -35,25 +35,66 @@ class Tree{
         System.out.println("ELEMENT IS INSERTED SUCCESSFULLY...");
     }
 
-    public void Search(String s){
+    public boolean Search(String s){
         node cur=root;
       for(int i=0;i<s.length();i++){
         node temp=cur.a.get(s.charAt(i));
         if(temp==null){
             System.out.println("ELEMENT NOT PRESENT IN THE TREE !!");
-            return;
+            return false;
         }
         cur=temp;
        }
        if(cur.endOfString==true){
         System.out.println("ELEMENT PRESENT IN THE TREE ...");
-        return;
+        return true;
        }else{
         System.out.println("ELEMENT NOT PRESENT IN THE TREE !! BUT IT IS PREFIX OF ANOTHER WORD");
-        return;
+        return false;
        }
     }
 
+    private boolean delete(node root,String word,int indx){
+        char ch=word.charAt(indx);
+        node cur=root.a.get(ch);
+        boolean deletenode;
+
+        if(cur.a.size()>1){
+            delete(cur, word, indx+1);
+            return false;
+        }
+        if(indx==word.length()-1){
+            if(cur.a.size()>=1){
+                cur.endOfString=false;
+                return false;
+            }else{
+                root.a.remove(ch);
+                return true;
+            }
+        }
+
+        if(cur.endOfString==true){
+            delete(cur, word, indx+1);
+            return false;
+        }
+
+        deletenode=delete(cur, word, indx+1);
+
+        if(deletenode==true){
+            root.a.remove(ch);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void delete(String word){
+        if(Search(word)){
+            delete(root, word, 0);
+        }else{
+            System.out.println("THEIR IS NO SUCH ELEMENT IN A TREE !!");
+        }
+    }
 
 }
 
@@ -64,6 +105,8 @@ public class trie {
         t.insert("NISCHAY");
         t.insert("VAIBHAV");
         t.insert(("MAHADEV"));
-        t.Search("NIs");
+        t.Search("VAIBHAV");
+        t.delete("VAIBHAV");
+        t.Search("VAIBHAV");
     }
 }

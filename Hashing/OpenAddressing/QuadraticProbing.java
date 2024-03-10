b47f76128f576e5da7f5d9728918b2888ad2a0d4
NischayHR-11 <nischayhr11@gmail.com>
2024-03-11 00:00:47 +0530

    INSERT ,DISPLAY AND SEARCH METHODS ARE IMPLEMENTED IN QUADRATIC PROBING

--- Hashing/OpenAddressing/QuadraticProbing.java
+++ Hashing/OpenAddressing/QuadraticProbing.java
@@ -41,20 +41,58 @@ class QP{
             int index=HashFunction(word);
             int count=0;
             for(int i=index;i<index+HashTable.length;i++){
-                int newindex=(i%HashTable.length)+(count*count);
+                int newindex=(index+(count*count))%HashTable.length;
                 if(HashTable[newindex]==null){
                     HashTable[newindex]=word;
-                    System.out.println("ELEMENT INSERTED SUCCESSFULLY..");
+                    System.out.println("ELEMENT INSERTED SUCCESSFULLY.. AT INDEX :"+newindex);
                     return;
                 }
+                count++;
             }
         }
+        UsedWords++;
     }
-    
+
+    public void display(){
+
+        System.out.println("------------------ HASH TABLE -----------------");
+        for(int i=0;i<HashTable.length;i++){
+            System.out.println("INDEX "+i+" , "+"KEY : "+HashTable[i]);
+        }
+        System.out.println("-----------------------------------------------");
+    }
+
+    public boolean search(String word){
+        int index=HashFunction(word);
+        int count=0;
+        if(HashTable[index]==null){
+            System.out.println(word+" NOT FOUND IN THE HASH TABLE !!");
+            return false;
+        }
+        for(int i=index;i<index+HashTable.length;i++){
+            int newindex=(index+(count*count))%HashTable.length;
+            if(HashTable[newindex]!=null && HashTable[newindex]==word){
+                System.out.println(word +" FOUND AT INDEX : "+newindex);
+                return true;
+            }
+        }
+        System.out.println(word+" NOT FOUND IN THE HASH TABLE !!");
+        return false;
+
+    }
 }
 
 public class QuadraticProbing {
     public static void main(String[] args) {
         
+        QP QuadraticProbing=new QP(10);
+        QuadraticProbing.insert("HI");
+        QuadraticProbing.insert("hello");
+        QuadraticProbing.insert("Q");
+        QuadraticProbing.insert("I");
+         QuadraticProbing.insert("AM");
+        QuadraticProbing.display();
+        QuadraticProbing.search("Q");
+
     }
 }

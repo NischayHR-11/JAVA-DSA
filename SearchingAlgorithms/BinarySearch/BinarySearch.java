df15bd08413c84145aacb5bd7db66de97913e646
NischayHR-11 <nischayhr11@gmail.com>
2024-03-15 01:57:18 +0530

    Binary Search Algorithm Implemented And verified With Output

---
+++ SearchingAlgorithms/BinarySearch/BinarySearch.java
@@ -0,0 +1,40 @@
+package SearchingAlgorithms.BinarySearch;
+
+// Time Complexity : O(nlog(n));
+
+// It Is Only Applicable For Sorted Data Structure.
+ 
+public class BinarySearch {
+
+    public static int BinarySearching(int a[],int key){
+
+        int beg=0;
+        int end=a.length-1;
+
+        while(beg<end){
+
+            int mid=(beg+end)/2;
+
+            if(a[mid]==key){
+                System.out.println("ELEMENT FOUND IN THE ARRAY AT INDEX : "+mid);
+                return mid;
+            }else if(key>a[mid]){
+                beg=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+
+        System.out.println("ELEMENT NOT FOUND IN THE ARRAY !!");
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+        int a[]={3,4,5,6,7,8,9,10};
+        BinarySearching(a, 4);
+        BinarySearching(a, 11);
+
+    }
+    
+}

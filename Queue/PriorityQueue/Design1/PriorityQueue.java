600b5c794505e28b6b1307834dd907097728b7fb
NISCHAY HR <nischayhr11@gmail.com>
2024-03-21 19:20:25 +0530

    Priority Queue Design 1 is implemented and verified

---
+++ Queue/PriorityQueue/Design1/PriorityQueue.java
@@ -0,0 +1,100 @@
+package Queue.PriorityQueue.Design1;
+
+class queue{
+
+    int front,rear;
+    int size;
+    int a[];
+
+    queue(int size){
+
+        front=rear=-1;
+        this.size=size;
+        a=new int [size];
+
+    }
+
+    void enqueue(int val){
+
+        if(rear==size-1){
+            System.out.println("ENQUEUE NOT POSSIBLE !! QUEUE IS FULL");
+            return;
+        }
+
+        if(front==-1)
+        front=0;
+
+        int i=rear;
+
+        for(;i>=front && a[i]>val;i--){
+
+            a[i+1]=a[i];
+        }
+
+        a[i+1]=val;
+        rear++;
+    }
+
+    int dequeue(){
+
+        if(front==-1 && rear==-1){
+            System.out.println("DEQUEUE NOT POSSIBLE !! QUEUE IS EMPTY");
+            return -1;
+        }
+
+        int n=a[front];
+
+        if(front==rear){
+
+            front=rear=-1;
+
+        }else{
+
+            front++;
+        }
+
+        return n;
+    }
+
+    int peek(){
+
+        if(front==-1 && rear==-1){
+            System.out.println("DEQUEUE NOT POSSIBLE !! QUEUE IS EMPTY");
+            return -1;
+        }
+
+        return a[front];
+
+    }
+
+    void display(){
+
+        for(int i=front;i<=rear;i++){
+
+            System.out.print(a[i]+" ");
+        }
+
+        System.out.println();
+    }
+
+}
+
+public class PriorityQueue {
+
+    public static void main(String[] args) {
+
+        queue q=new queue(3);
+
+        q.enqueue(3);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(4);
+        q.display();
+        System.out.println("DEQUEUED ELEMENT IS : "+q.dequeue());
+        System.out.println("DEQUEUED ELEMENT IS : "+q.dequeue());
+        System.out.println("DEQUEUED ELEMENT IS : "+q.dequeue());
+        System.out.println("DEQUEUED ELEMENT IS : "+q.dequeue());
+        
+    }
+    
+}

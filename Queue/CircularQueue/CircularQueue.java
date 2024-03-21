41f778b18d86ac7024574e599e9b9e138a5603cf
NISCHAY HR <nischayhr11@gmail.com>
2024-03-21 18:41:07 +0530

    circular is implemented and verified

--- Queue/CircularQueue/CircularQueue.java
+++ Queue/CircularQueue/CircularQueue.java
@@ -1,72 +1,74 @@
-class Circularqueue{
+package Queue.CircularQueue;
 
-    private int front,rear;
-    private int size;
-    private int a[];
+class queue{
 
-    public Circularqueue(int size){
+    int front,rear;
+    int size;
+    int a[];
 
+    queue(int size){
+
+        front=rear=-1;
         this.size=size;
         a=new int[size];
-        front=rear=-1;
+
     }
 
-
-    public void enqueue(int val){
+    void enqueue(int val){
 
         if((rear+1)%size==front){
+
             System.out.println("ENQUEUE NOT POSSIBLE !! QUEUE IS FULL");
             return;
         }
 
         if(front==-1)
         front=0;
-
+        
         rear=(rear+1)%size;
 
         a[rear]=val;
-
     }
 
-    public int dequeue(){
+    int dequeue(){
 
-        if(rear==-1 && front==-1){
-            System.out.println("DEQUEUE NOT POSIIBLE !! QUEUE IS EMPTY ");
+        if(rear ==-1 && front==-1){
+            System.out.println("DEQUEUE NOT POSSIBLE !! QUEUE IS EMPTY");
             return -1;
         }
 
         int n=a[front];
 
-        if(rear==front){
-        front=rear=-1;
+        if(front==rear){
+            front=rear=-1;
         }else{
             front=(front+1)%size;
         }
 
         return n;
+
     }
 
-    public int peek(){
-
-        if(rear==-1 && front==-1){
-            System.out.println("DEQUEUE NOT POSIIBLE !! QUEUE IS EMPTY ");
+    int peek(){
+      
+        if(rear ==-1 && front==-1){
+            System.out.println("DEQUEUE NOT POSSIBLE !! QUEUE IS EMPTY");
             return -1;
-        }
+        }  
 
         return a[front];
     }
 
-    public void display(){
 
-        int i=front;
+    void display(){
+
+        int i =front;
 
         while(i!=rear){
 
             System.out.print(a[i]+" ");
             i=(i+1)%size;
-
         }
-
         System.out.print(a[i]);
     }
 }
@@ -74,14 +76,17 @@ class Circularqueue{
 public class CircularQueue {
     
     public static void main(String[] args) {
-        
 
-        Circularqueue q=new Circularqueue(5);
+        queue q=new queue(3);
+
         q.enqueue(1);
         q.enqueue(2);
         q.enqueue(3);
         q.enqueue(4);
-        q.enqueue(5);
-        q.enqueue(6);
+        System.out.println("DEQUEUED ELEMENT IS : "+q.dequeue());
+        q.enqueue(0);
+        System.out.println(q.peek());
+        q.display();
+        
     }
 }

4f1c3d3cc928e3a05d86f39487c4deaef94e4e0e
NISCHAY HR <nischayhr11@gmail.com>
2024-05-04 13:49:44 +0530

    Quick Sort File Added To Divide And Conquer

---
+++ DivideAndConquer/QuickSort/QuickSort.java
@@ -0,0 +1,67 @@
+package DivideAndConquer.QuickSort;
+
+// Time Complexity Average Cases : O(nlog(n));  worst case : O(n^2);{If pivot Is Smallest Or Largest Element In Data Structure}
+
+// Space Complexity Average Cases : O(log(n));  Worst Case : O(n);{Recursion Tree is skewed Tree}
+
+// InPlace Sorting   And   UnStable Sorting;
+
+
+public class QuickSort {
+
+    public static int partition(int a[],int si,int ei){
+
+        int pivot=a[ei];
+        int i=si-1;
+
+        for(int j=si;j<ei;j++){
+            if(a[j]<pivot){
+                i++;
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+
+        i++;
+        int temp=a[i];
+        a[i]=a[ei];
+        a[ei]=temp;
+
+        return i;
+    }
+
+    public static void QuickSorting(int a[],int si,int ei){
+
+        if(si<ei){
+
+            int pivotIndex=partition(a, si, ei);
+
+            QuickSorting(a, si, pivotIndex-1);
+            QuickSorting(a, pivotIndex+1, ei);
+        }
+
+
+    }
+
+    public static void display(int a[]){
+
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+
+        int a[]={7,2,5,1,8,4,6,3};
+        System.out.println("Present Array : ");
+        display(a);
+        QuickSorting(a,0,a.length-1);
+        System.out.println("Sorted Array : ");
+        display(a);
+        
+    }
+    
+}

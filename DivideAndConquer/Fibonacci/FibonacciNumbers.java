aa5314cb93c88131c7478593369fbcd0939a4721
NISCHAY HR <nischayhr11@gmail.com>
2024-05-04 14:01:52 +0530

    Fibnoacci number File Added to Divide And Conquer

---
+++ DivideAndConquer/Fibonacci/FibonacciNumbers.java
@@ -0,0 +1,21 @@
+package DivideAndConquer.Fibonacci;
+
+public class FibonacciNumbers {
+
+    public static int fib(int n){
+
+        if(n==0 ||n==1)
+        return 0;
+
+        if(n==2)
+        return 1;
+
+        return fib(n-1)+fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("FIBONACCI OF 5TH TERM : "+fib(5));
+    }
+    
+}
